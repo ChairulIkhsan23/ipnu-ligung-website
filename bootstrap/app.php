fe7c5618aeffc60aa725f1ecc263dc031459e1cf
ChairulIkhsan <chairulikhsan23@gmail.com>
2026-02-11 18:15:10 +0700

    fix throttle api

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,10 +15,10 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->group('api', [
             EnsureFrontendRequestsAreStateful::class,
-            'throttle:api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
     ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
-    })->create();
+    })
+    ->create();
